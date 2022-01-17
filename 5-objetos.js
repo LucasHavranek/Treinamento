d54859ee5fd335e1d34732b0cbaaac04ce2667d1
LucasHavranek <81269366+LucasHavranek@users.noreply.github.com>
2022-01-16 23:50:00 -0300

    objetos

---
+++ 5-objetos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: "João",
+    idade: 38,
+    cpf:"12345678911",
+    email: "joaodascouves@gmail.com",
+    telefones: ["551190909090", "551180808080"],
+    dependentes: {nome: "Sara das Couves", parentesco: "filha", dataNasc:"20/03/2012"}
+}
+
+console.log(cliente)
